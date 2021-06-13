171326b64bc731aefa244ca589145b31755b8c26
zardoy <zardoy@users.noreply.github.com>
2021-06-13 00:34:55 +0000

    deploy: e5ffd142dd8eb865360ba7a0354066ca3ba2490c

---
+++ build/index.d.ts
@@ -0,0 +1,55 @@
+import { BrowserWindow, Rectangle } from "electron";
+import { Options as StoreOptions } from "electron-store";
+declare type PickedStoreOptions = Pick<StoreOptions<any>, "fileExtension" | "cwd" | "serialize" | "deserialize">;
+/**
+ * By default, this module trying to help you with tracking fileNames when creating new class instances and raising an **error** when you use the same file path twice.
+ *
+ * I think it shouldn't create any problems, until someone realise how to use HMR in Electron.
+ *
+ * Set this to `true` before the first class created to disable it.
+ * @default false */
+export declare let disableTrackingOfFileNames: boolean;
+interface Options extends PickedStoreOptions {
+    /**
+     * Set to `false`, to throw TypeError on loading invalid JSON config from file.
+     * @default true
+     */
+    clearInvalidConfig?: boolean;
+    /**
+     * Name of the file **without extension**, that stored in `cwd` option
+     * @default "window-state"
+     */
+    fileName?: string;
+    /** Set it to `false` to disable maximized state management. In this case you can use `maximze()` / `minimize()` on browserWindow directly
+     * @default It preserves window maximized state
+     */
+    maximized?: false | {
+        /**
+         * Whether window should be maximized by default (on the first run for example)
+         * @default true
+         *  */
+        default?: boolean;
+    };
+}
+declare const propsToPreserve: {
+    readonly numeric: readonly ["x", "y", "width", "height"];
+    readonly boolean: readonly ["fullscreen", "maximized"];
+};
+export default class ElectronWindowKeeper {
+    options: Options;
+    private electronStore;
+    /** Pass todo. It won't upate */
+    restoredState: Rectangle & {
+        fullscreen: boolean;
+    };
+    /** Including maximized */
+    restoredFullState: Rectangle & Record<(typeof propsToPreserve["boolean"])[number], boolean>;
+    filePath: string;
+    constructor(options?: Options);
+    manuallySaveState(browserWindow: BrowserWindow): void;
+    /**
+     * Like in electron-window-state, it sets maximized state (if not disabled) and watches for movement/resizing the window
+     */
+    manage(browserWindow: BrowserWindow): void;
+}
+export {};
---
+++ build/index.js
@@ -0,0 +1,87 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.disableTrackingOfFileNames = void 0;
+const electron_1 = require("electron");
+const electron_store_1 = __importDefault(require("electron-store"));
+const path_1 = __importDefault(require("path"));
+const lodash_1 = require("lodash");
+/**
+ * By default, this module trying to help you with tracking fileNames when creating new class instances and raising an **error** when you use the same file path twice.
+ *
+ * I think it shouldn't create any problems, until someone realise how to use HMR in Electron.
+ *
+ * Set this to `true` before the first class created to disable it.
+ * @default false */
+exports.disableTrackingOfFileNames = false;
+const propsToPreserve = {
+    numeric: ["x", "y", "width", "height"],
+    boolean: ["fullscreen", "maximized"]
+};
+const usedFilePaths = new Set();
+class ElectronWindowKeeper {
+    constructor(options = {}) {
+        var _a;
+        this.options = options;
+        // todo use lodash.defaults or something like that
+        const { clearInvalidConfig = true, fileName = "window-state", ...restOptions } = options;
+        const customFileOrPathSet = "fileName" in options || "cwd" in options;
+        const defaultCwd = electron_1.app.getPath("userData");
+        // https://github.com/sindresorhus/electron-store/tree/main/index.js#L60
+        const cwd = restOptions.cwd ?
+            path_1.default.isAbsolute(restOptions.cwd) ? restOptions.cwd : path_1.default.join(defaultCwd, restOptions.cwd) :
+            defaultCwd;
+        const filePath = path_1.default.join(cwd, `${fileName}.${(_a = restOptions.fileExtension) !== null && _a !== void 0 ? _a : "json"}`);
+        // todo learn english or how to specify more user-friendly messages
+        if (usedFilePaths.has(filePath))
+            throw new Error(`This file (${customFileOrPathSet ? "default values" : filePath}) was already used for another window. Please, specify another file name via "fileName" constructor option or disable tracking of used file names via "disableTrackingOfFileNames" named export.`);
+        usedFilePaths.add(filePath);
+        // try to type store
+        this.electronStore = new electron_store_1.default({
+            clearInvalidConfig,
+            name: fileName,
+            schema: {
+                ...Object.fromEntries(propsToPreserve.numeric.map(prop => [prop, {
+                        type: "number",
+                        minimum: 0
+                    }])),
+                ...Object.fromEntries(propsToPreserve.boolean.map(prop => [prop, {
+                        type: "boolean"
+                    }]))
+            },
+            ...restOptions
+        });
+        this.filePath = this.electronStore.path;
+        this.restoredFullState = lodash_1.pick(this.electronStore.store, [...propsToPreserve.numeric, ...propsToPreserve.boolean]);
+        this.restoredState = lodash_1.omit(this.restoredFullState, "maximized");
+    }
+    manuallySaveState(browserWindow) {
+        const bounds = browserWindow.getBounds();
+        this.electronStore.set({
+            ...bounds,
+            fullscreen: browserWindow.isFullScreen(),
+            maximized: browserWindow.isMaximized()
+        });
+    }
+    /**
+     * Like in electron-window-state, it sets maximized state (if not disabled) and watches for movement/resizing the window
+     */
+    manage(browserWindow) {
+        var _a;
+        // fullscreen controlled via prop
+        const { maximized: maximizedConfig } = this.options;
+        if (maximizedConfig !== false) {
+            if (this.restoredFullState.maximized)
+                browserWindow.maximize();
+            // todo test false option
+            else if (!("maximize" in this.restoredFullState) && ((_a = maximizedConfig === null || maximizedConfig === void 0 ? void 0 : maximizedConfig.default) !== null && _a !== void 0 ? _a : true))
+                browserWindow.maximize();
+        }
+        const updateState = lodash_1.debounce(() => this.manuallySaveState(browserWindow), 500);
+        browserWindow.on("move", updateState);
+        browserWindow.on("resize", updateState);
+    }
+}
+exports.default = ElectronWindowKeeper;
